d809c381073c912ff0ac455d7f2f12097be0be23
leandroandrade <leandro.andrade2588@gmail.com>
2021-04-13 18:38:17 -0300

    response api adjust

--- handler.js
+++ handler.js
@@ -1,57 +1,23 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
 module.exports.heroes = async (event) => {
     const heroes = [
-        {id:1, name: 'Batman'},
-        {id:2, name: 'Superman'},
+        { id:1, name: 'Batman' },
+        { id:2, name: 'Superman' },
     ]
+
     return {
         statusCode: 200,
-        body: JSON.stringify(
-            {
-                result: heroes,
-            },
-            null,
-            2
-        ),
+        body: JSON.stringify({ result: heroes }),
     };
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 module.exports.postHeroes = async (event) => {
-    console.log(event.body);
-
+    const { name } =  JSON.parse(event.body);
+    
     return {
         statusCode: 201,
-        body: JSON.stringify(
-            {
-                result: "Hero created",
-            },
-            null,
-            2
-        ),
+        body: JSON.stringify({ result: `Hero ${name} created!` })
     };
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
